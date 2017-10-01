c877e728bac2b7288b1909ed16ab2f38a5960a46
k-koehler <kevin.koehler@usask.ca>
2017-10-01 15:46:19 -0600

    added script for playermovment (NOT WORKING TODO)

---
+++ Accelerated Looters/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rbody;
+	private Animator animation;
+
+	public float jump;
+	public float movement;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+		animation = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump"))
+		{
+			rbody.AddForce (new Vector2 (0, jump));
+		}
+}
+
+	void FixedUpdate(){
+		var h = Input.GetAxis ("Horizontal");
+		rbody.AddForce (new Vector2 (h * movement, 0));
+		animation.SetFloat("Speed", Mathf.Abs (h));
+	}
+}
