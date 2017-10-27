5e5b42381b2def6ea0c1e771d753bdc60171e0e2
monica1995555 <monica.yiyuanweng@usask.ca>
2017-10-26 23:04:35 -0600

    add a shooting enemy

---
+++ Accelerated Looters/Assets/BulletTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.CrashReportHandler;
+
+public class BulletTest : MonoBehaviour
+{
+	public LevelManager MyLevelManager;
+
+	
+
+	public bool crash;
+	
+	public float maxDistance;
+
+
+
+
+	
+	
+	public Vector3 SoundInstantiate;
+
+	public GameObject clone;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+		
+	
+		MyLevelManager=FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			
+		
+		transform.Translate(10f*Time.deltaTime,0f,0f);
+		maxDistance += 2f*Time.deltaTime;
+		if (maxDistance >= 2)
+		{
+			DestroyBullet();
+		}
+
+		
+	}
+
+
+	public void OnTriggerEnter2D(Collider2D other)
+		{
+			
+		if ((other.CompareTag("Player")))
+		{
+			MyLevelManager.HurtPlayer(1);
+
+			
+			DestroyBullet();
+		}
+		else
+		{
+			return;
+		}
+		
+	}
+	
+
+
+	void DestroyBullet()
+	{
+		
+		Destroy(gameObject);
+	}
+	
+
+	
+}
---
+++ Accelerated Looters/Assets/Shooting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+	public GameObject TheBulletStart;
+
+	public GameObject Bullet;
+
+	public float ShootWaitTime;
+
+	public float BulletSpeed;
+	
+	public bool Shootable;
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		Shootable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Shootable)
+		{
+			shoot();
+			StartCoroutine(waitToShootAgain());
+		}
+		
+
+	}
+	
+	
+	void shoot()
+	{
+		Instantiate(Bullet.transform, TheBulletStart.transform.position, transform.rotation);
+	
+		
+
+	}
+	
+	IEnumerator waitToShootAgain()
+	{
+		Shootable = false;
+		yield return new WaitForSeconds(3f);
+		Shootable = true;
+	}
+
+
+}
