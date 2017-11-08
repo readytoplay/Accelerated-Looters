c82064b8d746feda7e6dee59862d90f147304b84
Koehler <kak750@usask.ca>
2017-11-08 10:21:30 -0600

    procedural stuff working somewhat now

--- Accelerated Looters/Assets/Assets/Scripts/ProceduralController.cs
+++ Accelerated Looters/Assets/Assets/Scripts/ProceduralController.cs
@@ -8,14 +8,12 @@ public class ProceduralController : MonoBehaviour {
     public GameObject gO;
 
     private Transform _playerTransform;
-    private const float FLOOR_Y = -4.86f, START_X = -9.5f, CHUNK_WIDTH = 10f; //TODO find right width
-    private float BOX_WIDTH;
+    private const float FLOOR_Y = -4.86f, BOX_WIDTH = 0.8f, START_X = -9.5f, CHUNK_WIDTH = 10f; //TODO find right width
 
 	// Use this for initialization
 	void Start () {
         _playerTransform = GameObject.FindWithTag("Player").transform;
         _generateFloor();
-        BOX_WIDTH = gO.GetComponent<BoxCollider2D>().bounds.size.magnitude;
     }
 
     private void _generateFloor()
@@ -31,10 +29,10 @@ public class ProceduralController : MonoBehaviour {
     /// <param name="l"></param>
     private void _generatePiece(float x, float y, float len)
     {
-        var cur_length = 0f;
-        while (cur_length < len)
+        var cur_length = x;
+        for (var i = 0; i<len; ++i)
         {
-            Instantiate(gO, new Vector3(x+BOX_WIDTH, y, 0), Quaternion.identity);
+            Instantiate(gO, new Vector3(cur_length, y, 0), Quaternion.identity);
             cur_length += BOX_WIDTH;
         }
     }
