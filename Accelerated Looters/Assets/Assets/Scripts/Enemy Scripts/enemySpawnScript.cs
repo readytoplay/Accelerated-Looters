9b4f669f82f733e5cfb37ba730212f200de43d97
kin452 <32176959+kin452@users.noreply.github.com>
2017-11-23 15:24:26 -0600

    adding a enemy spawning spot

---
+++ Accelerated Looters/Assets/Assets/Scripts/Enemy Scripts/enemySpawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class enemySpawnScript : MonoBehaviour {
+
+	public GameObject enemy;
+	public Transform[] spawnPoints;
+	private float timer;
+	public int spawnSpot;
+	// Use this for initialization
+
+	void Awake(){
+		timer = Time.time;
+	}
+
+	void Update (){
+
+		if(timer<Time.time){		//cooldown
+			int spawnPointIndex=Random.Range(0,spawnPoints.Length);
+			spawnSpot = spawnPointIndex;
+			Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			timer = Time.time + 10;
+		}
+
+
+
+	}
+}
