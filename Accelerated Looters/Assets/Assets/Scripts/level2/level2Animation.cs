9df7e934faab0f4982389b3ece4a927cce4a64e7
monica1995555 <monica.yiyuanweng@usask.ca>
2017-11-25 16:10:49 -0600

    Added animation for level 2

---
+++ Accelerated Looters/Assets/Assets/Scripts/level2/level2Animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level2Animation : MonoBehaviour
+{
+
+	public playerController p;
+
+	public Animator a;
+
+	public Rigidbody2D MyRigidbody2D;
+
+	public float velocity;
+
+	public bool isGrounded;
+
+	// Use this for initialization
+	void Start ()
+	{
+		p = FindObjectOfType<playerController>();
+		a = GetComponent<Animator>();
+		MyRigidbody2D = p.myRigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		velocity =Mathf.Abs( MyRigidbody2D.velocity.x);
+		isGrounded = p.isGrounded;
+		a.SetFloat("Speed", velocity);
+		a.SetBool("Grounded", isGrounded);
+	}
+}
