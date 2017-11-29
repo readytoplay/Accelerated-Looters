99674cdde4393d959a89fc1c8d1b96f421db5570
Koehler <kak750@usask.ca>
2017-11-29 16:02:37 -0600

    procedural camera fixed

---
+++ Accelerated Looters/Assets/Assets/Scripts/Procedural Scripts/CameraFollowProcedural.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowProcedural : MonoBehaviour
+{
+    [SerializeField]
+    public Transform targetTransform;
+
+    void LateUpdate()
+    {
+        var tempVec3 = new Vector3();
+        tempVec3.x = targetTransform.position.x;
+        tempVec3.y = this.transform.position.y;
+        tempVec3.z = this.transform.position.z;
+        this.transform.position = tempVec3;
+    }
+}
