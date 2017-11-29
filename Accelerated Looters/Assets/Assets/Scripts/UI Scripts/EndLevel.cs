ce31887156e7390086a5e57f6430148a2d7c6e57
Daylon Ball <daylonball96@gmail.com>
2017-11-29 09:36:07 -0600

    Changed a few UI things.

---
+++ Accelerated Looters/Assets/Assets/Scripts/UI Scripts/EndLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour {
+
+    static public int coins;
+    public GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(coins);
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene("End Level Menu");
+        }
+    }
+
+}
