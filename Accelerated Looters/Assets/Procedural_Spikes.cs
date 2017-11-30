4ae0a8db549ccc76cc96b5d1435de03a9dc7349e
Matthew <mas487@mail.usask.ca>
2017-11-30 12:40:08 -0600

    Procedural Spikes
    Spikes in procedural level now work

---
+++ Accelerated Looters/Assets/Procedural_Spikes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Procedural_Spikes : MonoBehaviour {
+
+    public playerController player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<playerController>(); //gets script to access
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+                player.spikeDamage();
+                player.myRigidbody.velocity = new Vector3(player.myRigidbody.velocity.x, player.jumpSpeed + 5, 0);
+    }
+}
