a60bb0b7807d605b7b771d90dd27e5b4cde7ffa1
monica_wena_pal_pisco <monica.yiyuanweng@usask.ca>
2017-11-30 13:49:44 -0600

    coins working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1

---
+++ Accelerated Looters/Assets/coinUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class coinUI : MonoBehaviour {
+	public int totalCoins;
+		public Text coins;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totalCoins = PlayerPrefs.GetInt("totalcoins");
+		
+		coins.text = (" " +totalCoins+ " ");
+	}
+}
